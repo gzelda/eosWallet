85a2839b93ea85ba4886e2f4277ac5318b5cf585
tygavin <tianyao@zju.edu.cn>
2019-01-17 07:29:20 +0800

    修改CPU NET交易

--- routes/eosBank/trxCPU.js
+++ routes/eosBank/trxCPU.js
@@ -19,44 +19,50 @@ router.post('/', function(req, resp, next) {
 	//读db获取UID和私钥
 	db.getRow(UID, function(data){
 		console.log(data);
-		if (data != "error"){
-			console.log(data.accountName);
-			console.log(data.activePriKey);
-			var userPriKey = data.ownerPriKey;
-			var userAccountName = data.accountName;
-			var eos = Eos({
-			//payer的私钥
-			    keyProvider: userPriKey,// private key
-			    httpEndpoint: config.chainServer,
-			    //http://api.eosbeijing.one
-			    chainId: config.chainId
-			});
-
-
-			var fromAccount = userAccountName;
-			var toAccount = "cpubankeosio";
-			//读redis TODO
-			var eosAmount = "0.0200 EOS";
-			var memo = "1d " + fromAccount +" cpu";
-				//console.log(amount.toFixed(4) + " EOS");
-			console.log("in");
-		    eos.transaction(tr => {
-		    	console.log("in2");
-				tr.transfer(fromAccount,toAccount,eosAmount,memo);
-			})
-			.then(r => {
-					//返回成功结果
-					console.log(r);
-					resp.send(respJson.generateJson(1,0,"购买成功",r));
-				}).catch(e => {
-					//返回失败结果
-					console.log(e);
-					resp.send(respJson.generateJson(0,0,"链端购买失败，请检查余额、cpu等相关参数",e));
+		switch(data)
+		{
+		    case "void":
+		    	resp.send(respJson.generateJson(0,0,"此UID没有eos钱包"));
+		        break;
+		    case "error":
+		    	console.log("error db");
+				resp.send(respJson.generateJson(0,0,"读库失败"));
+		        break;
+		    default:
+		    	console.log(data.accountName);
+				console.log(data.activePriKey);
+				var userPriKey = data.ownerPriKey;
+				var userAccountName = data.accountName;
+				var eos = Eos({
+				//payer的私钥
+				    keyProvider: userPriKey,// private key
+				    httpEndpoint: config.chainServer,
+				    //http://api.eosbeijing.one
+				    chainId: config.chainId
 				});
-		}
-		else{
-			console.log("error db");
-			resp.send(respJson.generateJson(0,0,"读库失败，此UID没有eos钱包"));
+
+
+				var fromAccount = userAccountName;
+				var toAccount = "cpubankeosio";
+				//读redis TODO
+				var eosAmount = "0.0200 EOS";
+				var memo = "1d " + fromAccount +" cpu";
+					//console.log(amount.toFixed(4) + " EOS");
+				console.log("in");
+			    eos.transaction(tr => {
+			    	console.log("in2");
+					tr.transfer(fromAccount,toAccount,eosAmount,memo);
+				})
+				.then(r => {
+						//返回成功结果
+						console.log(r);
+						resp.send(respJson.generateJson(1,0,"购买成功",r));
+					}).catch(e => {
+						//返回失败结果
+						console.log(e);
+						resp.send(respJson.generateJson(0,0,"链端购买失败，请检查余额、cpu等相关参数",e));
+					});
+
 		}
 	});
 	
